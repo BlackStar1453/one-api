37ee54dcc7054bea5eb3f6c88342b02936eefe7f
BlackStar1453 <69788237+BlackStar1453@users.noreply.github.com>
2024-10-09 00:58:49 +0800

    修复忘记导入的错误

--- middleware/utils.go
+++ middleware/utils.go
@@ -6,6 +6,7 @@ import (
 	"github.com/songquanpeng/one-api/common"
 	"github.com/songquanpeng/one-api/common/helper"
 	"github.com/songquanpeng/one-api/common/logger"
+	"github.com/songquanpeng/one-api/model"
 	"strings"
 )
 
